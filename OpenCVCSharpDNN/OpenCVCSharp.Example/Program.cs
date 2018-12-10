6f40f0b0a1d6a989496df1b38c3c7f1b4ef455cd
julosp <julosp@eltiempo.com>
2018-12-10 13:54:55 -0500

    Add OpenCVCSharpDNN implementation YoloV3, Caffe

---
+++ OpenCVCSharpDNN/OpenCVCSharp.Example/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCVCSharpDNN.Impl;
+using OpenCVCSharpDNN;
+using System.IO;
+namespace OpenCVCSharp.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
